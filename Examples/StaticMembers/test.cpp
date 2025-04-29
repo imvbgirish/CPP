35cb026f0e167d1f40626c6745bfadbc6954f653
imvbgirish <girishvb26@gmail.com>
2025-04-29 15:06:21 +0530

    Practiced pointer,referenceVar and class by cherno

--- Examples/StaticMembers/test.cpp
+++ Examples/StaticMembers/test.cpp
@@ -6,10 +6,12 @@ int test::count = 1;
 void test::increment(){
     count++;
     std::cout << count << std::endl;
+    //name = "Girish";  --->  //Throws error
 }
 
 void test::printNum(){
     for(int i=0; i<10; i++){
         std::cout << i << " ";
     }
+    name = "Girish";   //---> Works
 }
